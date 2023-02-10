7983b03f4de703d5a78e3c50ed9fa4da545058f2
flyinglimao <hi@limaois.me>
2023-02-10 20:44:11 +0800

    feat: navbar

---
+++ app/core/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react"
+
+const Navbar: FC = () => {
+  return (
+    <nav className="bg-[#171717] sticky top-0">
+      <div className="mx-auto px-6 py-4 flex justify-between">
+        <span className="text-white font-extrabold text-3xl">feadbeats</span>
+        <button className="h-9 w-36 rounded-full text-white border-white border font-light">
+          Sign up
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,14 +1,19 @@
 import Head from "next/head"
-import React, {FC} from "react"
-import {BlitzLayout} from "@blitzjs/next"
+import React from "react"
+import { BlitzLayout } from "@blitzjs/next"
+import Navbar from "../components/Navbar"
 
-const Layout: BlitzLayout<{title?: string; children?: React.ReactNode}> = ({title, children}) => {
+const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
+  title,
+  children,
+}) => {
   return (
     <>
       <Head>
         <title>{title || "feadbeats"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
 
       {children}
     </>
