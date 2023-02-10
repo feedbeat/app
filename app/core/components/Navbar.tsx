192deb6b9d249b951efa1771a1ac6a9e7fc4f0dd
flyinglimao <hi@limaois.me>
2023-02-11 01:49:17 +0800

    feat: landing page

--- app/core/components/Navbar.tsx
+++ app/core/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 
 const Navbar: FC = () => {
   return (
-    <nav className="bg-[#171717] sticky top-0">
+    <nav className="bg-[#171717] w-full fixed top-0">
       <div className="mx-auto px-6 py-4 flex justify-between">
         <span className="text-white font-extrabold text-3xl">feadbeats</span>
         <button className="h-9 w-36 rounded-full text-white border-white border font-light">
