e8be4e06e633eabaa0553dafdebb05af501c714b
flyinglimao <hi@limaois.me>
2023-02-12 02:49:49 +0800

    feat: add user component

--- app/core/components/Navbar.tsx
+++ app/core/components/Navbar.tsx
@@ -1,16 +1,28 @@
+import { useConnectModal } from "@rainbow-me/rainbowkit"
 import Link from "next/link"
 import { FC } from "react"
+import { useAccount } from "wagmi"
+import User from "./User"
 
 const Navbar: FC = () => {
+  const { address } = useAccount()
+  const { openConnectModal } = useConnectModal()
   return (
     <nav className="bg-[#171717] w-full fixed top-0">
       <div className="mx-auto px-6 py-4 flex justify-between">
-        <span className="text-white font-extrabold text-3xl">feadbeats</span>
-        <Link href="/signup" passHref legacyBehavior>
-          <a className="h-9 w-36 rounded-full text-white border-white border font-light text-center py-1">
-            Sign up
-          </a>
+        <Link href="/" passHref legacyBehavior>
+          <a className="text-white font-extrabold text-3xl">feadbeats</a>
         </Link>
+        {address ? (
+          <User />
+        ) : (
+          <button
+            className="h-9 w-36 rounded-full text-white border-white border font-light"
+            onClick={openConnectModal}
+          >
+            Sign up
+          </button>
+        )}
       </div>
     </nav>
   )
---
+++ app/core/components/User.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+import Image from "next/image"
+import { FC } from "react"
+import { useAccount, useEnsAvatar, useEnsName } from "wagmi"
+
+const User: FC = () => {
+  const { address } = useAccount()
+  const { data: avatar } = useEnsAvatar({ address })
+  const { data: ensName } = useEnsName({ address })
+  return (
+    <Link href="/profile" passHref legacyBehavior>
+      <a className="inline-flex items-center -my-[1rem]">
+        <div className="rounded-full w-[3rem] h-[3rem] overflow-hidden border-4 border-[#36322D] box-content">
+          <Image src={avatar || "/default-avatar.jpg"} width={48} height={48} alt="User avatar" />
+        </div>
+        <div className="rounded-full bg-[#242221] -ml-[2rem] h-[3rem] w-[11.5rem] pl-[2.25rem] flex flex-col justify-center">
+          <p className="text-white font-semibold text-xl leading-5">{ensName}</p>
+          <p className="text-[#FF7A2F] font-bold text-sm leading-4">
+            {address?.slice(0, 4) + "..." + address?.slice(-4)}
+          </p>
+        </div>
+      </a>
+    </Link>
+  )
+}
+
+export default User
