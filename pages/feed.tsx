c867b4db5a51cb6bba37f13b90e4cda361320a37
flyinglimao <hi@limaois.me>
2023-02-13 14:24:05 +0800

    feat: paginatiojn component

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,10 +1,13 @@
 import Account from "@/app/core/components/Account"
 import Activity from "@/app/core/components/Activity"
+import Pagination from "@/app/core/components/Pagination"
 import { BlitzPage } from "@blitzjs/next"
 import Layout from "app/core/layouts/Layout"
+import { useState } from "react"
 import { IoAddOutline } from "react-icons/io5"
 
 const Feed: BlitzPage = () => {
+  const [page, setPage] = useState(1)
   return (
     <Layout>
       <div className="min-h-full bg-cover bg-bottom text-white pt-[8.25rem] px-12 pb-16 flex flex-wrap flex-row justify-between">
@@ -24,6 +27,14 @@ const Feed: BlitzPage = () => {
               }}
             />
           </div>
+          <Pagination
+            className="pl-16 mt-10"
+            rangeStart={1}
+            rangeEnd={6}
+            end={8}
+            onGo={setPage}
+            current={page}
+          />
         </div>
         <div className="w-[32.5rem] mt-2 text-[#2F2F2F]">
           <div className="bg-white rounded-lg">123</div>
