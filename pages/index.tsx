5923a1ef7156542493ce63fe72a2d69478b73988
flyinglimao <hi@limaois.me>
2023-02-15 16:40:30 +0800

    feat: landing cta btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import useUser from "@/app/core/hooks/useUser"
 import { BlitzPage } from "@blitzjs/next"
 import Layout from "app/core/layouts/Layout"
+import { useRouter } from "next/router"
 
 const Home: BlitzPage = () => {
+  const { startLogin, isLoggedIn } = useUser()
+  const router = useRouter()
   return (
     <Layout>
       <div
@@ -13,7 +17,13 @@ const Home: BlitzPage = () => {
         <div>
           <p className="font-extrabold text-[4rem] leading-[4.75rem]">Web3 Feed For All Gamers</p>
           <p className="text-4xl font-light mb-6">See what games alpha users play</p>
-          <button className="rounded-full w-[16.5rem] h-[4rem] bg-[#FF2E00] font-extrabold text-2xl">
+          <button
+            className="rounded-full w-[16.5rem] h-[4rem] bg-[#FF2E00] font-extrabold text-2xl"
+            onClick={() => {
+              if (isLoggedIn) router.push("/feed").catch(console.error)
+              else startLogin()
+            }}
+          >
             Start feedbeats
           </button>
         </div>
